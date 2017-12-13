e0ec73569b3d7e9b5f74cdbc3f91bb12ed7a6f6c
liudas4test <liudas.keturi@outlook.com>
2017-12-13 15:56:08 +0000

    Creating constructors and prototypes lesson

--- JS_Objects_and_functions_learning.js
+++ JS_Objects_and_functions_learning.js
@@ -1,4 +1,29 @@
-console.log('testas testas test');
-//-----new branch
-console.log('new branch. Test one');
-console.log('one');
+//-------------creating objects--------------
+
+/*var testPerson = {
+    name: 'Bird',
+    yearOfBirth: 1919,
+    job: 'jobless'
+};*/
+
+var Person = function(name, yearOfBirth, job){
+    this.name = name;
+    this.yearOfBirth = yearOfBirth;
+    this.job = job;
+    
+}
+
+Person.prototype.calculateAge = function(){
+    console.log(2017 - this.yearOfBirth);
+};
+
+Person.prototype.lastName = 'Tree';
+
+var testPerson = new Person('Bird',1912,'jobless');
+var secondTestPerson = new Person('Ell', 1933, 'retired');
+
+testPerson.calculateAge();
+secondTestPerson.calculateAge();
+
+console.log(testPerson.lastName);
+console.log(secondTestPerson.lastName);
